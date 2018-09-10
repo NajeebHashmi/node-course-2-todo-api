708f261b0f6212299feadb0ac937e1741df13e86
Najeeb Hashmi <Najeeb.Hashmi@thermofisher.com>
2018-09-10 13:55:17 -0700

    added files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -77,6 +78,32 @@ app.get('/todos/:id',(req, res) => {
         },(err)=>{
             res.status(400).send(err);
         });
+    });
+
+    app.patch('/todos/:id', (req, res) => {
+        var id = req.params.id;
+        var body = _.pick(req.body,['text','completed']);
+        if (!ObjectID.isValid(id))
+        {
+            return res.status(404).send();
+        }
+
+        if (_.isBoolean(body.completed) && body.completed) {
+            body.completedAT = new Date().getTime();
+            console.log(body);
+        } else {
+            body.completed = false;
+            body.completedAt = null
+        }
+
+        Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo) => {
+            if (!todo){
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch((e) => {
+            res.status(400).send();
+        })
     });
     
     
